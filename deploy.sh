0de83a19e7e7a838800f30c039446be473c68c00
Justin Tan <97202184+justan-kj@users.noreply.github.com>
2023-05-03 04:14:08 +0800

    Fixed port mapping

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 docker stop mod4-app || true
 docker rm mod4-app || true
 docker pull afawafa/mod4-project-docker:latest
-docker run -d --name mod4-app -p 8080:80 afawafa/mod4-project-docker:latest
+docker run -d --name mod4-app -p 8080:8080 afawafa/mod4-project-docker:latest
